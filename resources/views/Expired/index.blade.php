6c2f9654c1655f61994ebedc296988238ea2c848
Rian A <kindlead>
2020-02-16 16:40:22 +0700

    '

---
+++ resources/views/Expired/index.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-4">
+                <img src="https://5.imimg.com/data5/XQ/KP/MY-40305254/kids-toy-500x500.jpg" style="width:300px; height:350px;" alt="" srcset="">
+            </div>
+            <div class="col-8">
+                <div class="jumbotron bg-white">
+                    <h1 class="display-4">Upps Sorry !!</h1>
+                    <p class="lead"> You have been used all your trial Period for this Web Application, please kindly contact developer for continue Subcription !</p>
+                    <hr class="my-4">
+                    <p>Hi, <b>don't worry</b>, datas are saved, you just need to activate this Web Application and All be fine as used to !</p>
+                    <a class="btn btn-warning btn-lg" href="mailto:kitakitadevs@gmail.com?Subject=Hello, I want Continue Subcripting !" target="_top">Contact Developer</a>
+                  </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
