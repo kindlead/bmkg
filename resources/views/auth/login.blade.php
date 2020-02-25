9ceac4db953129a57d5ad2a7d42edcb9ea22165c
unknown <ingatphising@gmail.com>
2020-02-25 22:30:31 +0700

    bmkg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,8 +64,8 @@
                         {{ __('Forgot Your Password?') }}
                     </a>
                 @endif --}}
-                <p class="text-dark font-weight-medium text-center flex-grow align-self-end">Don't have any account?   <a class="text-info bold" href="{{ route('register') }}">Register!</a></p>
-                <p class="text-dark font-weight-medium text-center flex-grow align-self-end">Powered by : <a class="text-info underline bold" href="https://instagram.com/kindlead_">Kindlead</a></p>
+                <!-- <p class="text-dark font-weight-medium text-center flex-grow align-self-end">Don't have any account?   <a class="text-info bold" href="{{ route('register') }}">Register!</a></p>
+                <p class="text-dark font-weight-medium text-center flex-grow align-self-end">Powered by : <a class="text-info underline bold" href="https://instagram.com/kindlead_">Kindlead</a></p> -->
                 </div>
               </form>
             </div>
